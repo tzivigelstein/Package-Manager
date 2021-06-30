d0d1feece8f6620495b21c2f1968e4e062a06fb2
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-06-30 14:53:13 -0300

    Added dynamic uris

--- controllers/downloadsController.js
+++ controllers/downloadsController.js
@@ -1,13 +1,16 @@
 const fs = require('fs')
+require('dotenv').config()
 
 module.exports.downloadApk = (req, res) => {
   const { package } = req.headers
 
+  const url = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : process.env.DEVELOPMENT_URL
+
   const path = `./public/apk/${package}.apk`
   if (fs.existsSync(path)) {
-    const response = `http://192.168.0.10:4000/apk/${package}.apk`
-    res.status(200).json({ status: 'ok', statusCode: 200, response })
+    const response = `${url}/apk/${package}.apk`
+    res.status(200).json({ response })
   } else {
-    res.status(404).json({ status: 'bad', statusCode: 404 })
+    res.status(404).json({})
   }
 }
