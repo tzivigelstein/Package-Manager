43eee8622466ae3f3fcdca8f34fd9df112e8d41c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-06-30 13:55:25 -0300

    Fixed issue in public/apk

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,9 +2,15 @@ const fs = require('fs')
 const multer = require('multer')
 
 module.exports.uploadApk = (req, res) => {
+  const dir = __dirname + '/../public/apk'
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+
   const multerConfig = {
     storage: (fileStorage = multer.diskStorage({
-      destination: (req, file, callback) => callback(null, __dirname + '/../public/apk'),
+      destination: (req, file, callback) => callback(null, dir),
       filename: (req, file, callback) => {
         const packageName = `${req.headers.packagename}.apk`
         callback(null, packageName)
@@ -17,10 +23,10 @@ module.exports.uploadApk = (req, res) => {
   upload(req, res, async error => {
     if (!error) {
       console.log('File saved')
+      return res.status(200).json({})
     } else {
+      console.log(error)
       return res.status(409).json({ error })
     }
   })
-
-  return res.status(200).json({})
 }
